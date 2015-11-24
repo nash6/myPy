22b8bf66d711c63b360ea50adc0695528eabfc29
S6 <nash6@qq.com>
2015-11-24 16:33:53 +0800

    def func

--- PyLuaTblParser.py
+++ PyLuaTblParser.py
@@ -2,3 +2,21 @@
 #Written by lyc 2015.11.24
 #For Netease codefly
 
+
+class PyLuaTblParser(object):
+	def load(self, s):
+		pass
+	
+	def dump(self):
+		pass
+
+	def loadLuaTable(self, f):
+		pass
+	def dumpLuaTable(self, f):
+		pass
+
+	def loadDict(self, d):
+		pass
+
+	def dumpDict(self):
+		pass
